550ef97406f57c38b4fb8a849b460374eb2e6c49
Tyler Hall <thall2980@gmail.com>
2022-08-31 16:42:41 -0400

    commit tree 8fe0ce9ab81021379832d7f31c95ec260493ef4b

--- index.js
+++ index.js
@@ -11,6 +11,92 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = tutorials.map((currentValue, index)) => {
+
+//   return tutorials
+// }
+
+// tutorials.map () => {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// const capitalizeFirstLetter = string => {
+//   return string.charAt(0).toUpperCase() + string.slice(1)
+// }
+
+// const splitString = string => {
+//   return string.split(" ");
+// }
+
+// tutorials.forEach(index => {
+//   return splitString(index => {
+//     return capitalizeFirstLetter(index);
+//   }
+//     )
+// }
+// )
+
+
+
+
+// const testFunction = () => {
+// //  tutorials.forEach(index => {
+//    // return capitalizeFirstLetter(index);
+//    tutorials.forEach(index => {
+//     return splitString(index => {
+//       return capitalizeFirstLetter(index);
+//     }
+//       )
+//   }
+//   )
+// }
+// testFunction()
+
+
+// const titleCased = (index) => {
+//   let array1 = index.split(" ").map()
+//     return array1.charAt(0).toUpperCase();
+
+//   }
+  
+// }
+
+// function titleCased() {
+//   const cased = tutorials.map((cb) => {
+//       const newCase = cb
+//           .split(' ')
+//           .map((word) => {
+//               return word.charAt(0).toUpperCase() + word.slice(1)
+//           })
+//           .join(' ')
+
+//       return newCase
+//   })
+//   return cased
+// }
+
+
 const titleCased = () => {
-  return tutorials
-}
+    let lines = tutorials.map((cb) => {
+      let splitLines = cb
+        .split(' ')
+        .map((word) => {
+          return word.charAt(0).toUpperCase() + word.slice(1)
+        })
+        .join(' ')
+        return splitLines
+        })
+    return lines
+    }
+
